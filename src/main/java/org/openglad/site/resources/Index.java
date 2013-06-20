eb2773fea8d070ceaa6e80bc93910808c191243d
Sean Ford <sdford@gmail.com>
2013-06-19 22:56:30 -0700

    changed index page to redirect to news

--- src/main/java/org/openglad/site/resources/Index.java
+++ src/main/java/org/openglad/site/resources/Index.java
@@ -1,16 +1,21 @@
 package org.openglad.site.resources;
 
-import org.glassfish.jersey.server.mvc.Template;
-
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * @author Sean Ford
  * @since 2013-06-19
  */
 @Path("/")
-@Template
 public class Index
 {
-
+	@GET
+	public Response redirectToNews() throws URISyntaxException
+	{
+		return Response.seeOther(new URI("/news")).build();
+	}
 }
--- src/main/webapp/org/openglad/site/resources/Index/index.jsp
+++
@@ -1,7 +0,0 @@
-<jsp:include page="/includes/header.jsp" />
-
-My index page!
-<br/>
-Check out the <a href="/campaigns">latest campaigns<a/>!
-
-<jsp:include page="/includes/footer.jsp" />
